62c028c566dcd4ccf1aad00faa6ef4907fb7d431
chenyangguang <vim.09071@gmail.com>
2016-09-30 15:08:38 +0800

    * format

--- php/Captcha/decryption.php
+++ php/Captcha/decryption.php
@@ -1,28 +1,27 @@
 <?php
     $decry_email = 'XDY1XDc2XDY5XDZjXDYzXDZmXDczXDQwXDY3XDZkXDYxXDY5XDZjXDJlXDYzXDZmXDZk';
-function decryption($decry_email)
-{
-    $real_email      = '';
-    $decry_email_arr = array_shift(explode('\\', base64_decode($decry_email)));
+    function decryption($decry_email)
+    {
+        $real_email      = '';
+        $decry_email_arr = array_shift(explode('\\', base64_decode($decry_email)));
 
-    foreach ($decry_email_arr as $val) {
-        $real_email .= chr(hexdec($val));
+        foreach ($decry_email_arr as $val) {
+            $real_email .= chr(hexdec($val));
+        }
+
+        return $real_email;
     }
 
-    return $real_email;
-}
+    function encrypt($str)
+    {
+        $origin_arr = str_split($str);
+        $encry_str  = '';
+        foreach ($origin_arr as $v) {
+            $encry_str .= base64_encode(dechex(ord($v)));
+        }
 
-
-function encrypt($str)
-{
-    $origin_arr = str_split($str);
-    $encry_str  = '';
-    foreach ($origin_arr as $v) {
-        $encry_str .= base64_encode(dechex(ord($v)));
+        return $encry_str;
     }
 
-    return $encry_str;
-}
-
-var_dump(decryption($decry_email));
-var_dump(encrypt('evilcos@gmail.com'));
+    var_dump(decryption($decry_email));
+    var_dump(encrypt('evilcos@gmail.com'));
