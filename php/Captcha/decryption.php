47ffdd180242340a216cf00e837d5dea5579808a
chenyangguang <vim.09071@gmail.com>
2016-09-30 14:43:42 +0800

    * function method

--- php/Captcha/decryption.php
+++ php/Captcha/decryption.php
@@ -1,14 +1,29 @@
 <?php
     $decry_email = 'XDY1XDc2XDY5XDZjXDYzXDZmXDczXDQwXDY3XDZkXDYxXDY5XDZjXDJlXDYzXDZmXDZk';
-    $decry_email = base64_decode($decry_email);
-    $decry_email_arr = explode('\\', $decry_email);
-
-    $real_email = '';
-    $purge_email = array_shift($decry_email_arr);
+function decryption($decry_email)
+{
+    $real_email      = '';
+    $decry_email_arr = array_shift(explode('\\', base64_decode($decry_email)));
 
     foreach ($decry_email_arr as $val) {
         $real_email .= chr(hexdec($val));
     }
 
-var_dump($real_email);
+    return $real_email;
+}
 
+
+function encrypt($str)
+{
+    $origin_arr = str_split($str);
+    $encry_str  = '';
+    foreach ($origin_arr as $v) {
+        echo base64_encode('\\'.dechex(ord($v))) . "\n";
+        $encry_str .= base64_encode(dechex(ord($v)));
+    }
+
+    return $encry_str;
+}
+
+var_dump(decryption($decry_email));
+var_dump(encrypt('evilcos@gmail.com'));
