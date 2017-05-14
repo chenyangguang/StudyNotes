2daa8418b3a6fb41502a4fb57938b3bf2b9c4df0
chenyangguang <vim.09071@gmail.com>
2017-05-14 23:14:32 +0800

    * php实现 二分查找法

---
+++ php/algorithm/binary_search.php
@@ -0,0 +1,23 @@
+<?php
+
+//  二分查找
+function binary_search($arr = [], $key)
+{
+    $end = count($arr);
+    $begin = 0;
+    if($end <= 0 || $begin > $end) return 0;
+
+    while($begin <= $end) {
+        $tmp_mid = intval(($begin + $end) / 2);
+        if($arr[$tmp_mid] == $key) return $tmp_mid;
+
+        if($arr[$tmp_mid] > $key) {
+            $end = $mid - 1; // 小于中间值，跑前面找
+        } else {
+            $begin = $tmp_mid + 1; // 大于中间值，跑后面去找
+        }
+    }
+}
+
+$arr = [11, 2, 43432, 22, 86, 210, 320, 223];
+var_dump(binary_search($arr, 86));
