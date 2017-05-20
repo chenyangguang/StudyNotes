3029cd54fff84dffaefd3403a4d33688cbef2c1e
chenyangguang <vim.09071@gmail.com>
2017-05-20 19:05:23 +0800

    * 优化随机数生成

--- php/program/createRandNumbers.php
+++ php/program/createRandNumbers.php
@@ -1,7 +1,17 @@
 <?php
-function  createRandNumbers($from, $to, $limit) {
-    
-    $arr = [];
+function  createRandNumbers($from = 0, $to = 0, $limit = 0)
+{
+    $arr   = [];
+    $from  = intval($from);
+    $to    = intval($to);
+    $limit = abs($limit);
+
+    if($from > $to) {
+        $tmp = $to;
+        $to = $from;
+        $from = $tmp;
+    }
+
     for ($i = 0; $i < $limit; ++$i) {
         $arr[] = mt_rand($from, $to);
     }
