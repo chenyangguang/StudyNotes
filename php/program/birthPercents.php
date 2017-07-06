f0e091ddcaa9c1fc6f826182e44afa6e1ab3928e
chenyangguang <vim.09071@gmail.com>
2017-07-06 21:56:02 +0800

    * 不考虑代数

--- php/program/birthPercents.php
+++ php/program/birthPercents.php
@@ -3,7 +3,7 @@
  * 生男生女比例是50%, 每个家庭生到第一个男孩就不再生，那么产生的男女比例是多少？
  * 
  */
-function calculateBirthPercents($base, $generations)
+function calculateBirthPercents($base)
 {
     $female = 0;
     $male = 0;
@@ -15,7 +15,7 @@ function calculateBirthPercents($base, $generations)
         $male++;
         $percents = $female / $male;
         if($i % 100 === 0) {
-            echo sprintf('calculate number: %d, total generations:%d, percents=%f',
+            echo sprintf('calculate number: %d, percents=%f',
                          $i, $generations, $percents), PHP_EOL;
         }
     }
@@ -27,6 +27,4 @@ function haveChildren() {
      return random_int(0,1);
 }
 
-
-
-var_dump(calculateBirthPercents(100000, 1));
+calculateBirthPercents(100000);
